5ede91e9ed05bf31fbcc87d723a9e0527de78e8d
zozo-url <zozo-url@protonmail.ch>
2019-06-24 15:35:47 +1200

    100gecs start

--- bot.js
+++ bot.js
@@ -1,10 +1,48 @@
-console.log('hey')
-
 var Twit = require('twit')
+// var exec = require('child_process').exec
+var config = require('./config')
+var T = new Twit(config)
+var fs = require('fs')
 
-var T = new Twit({
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...'
-})
+var counter = 1;
+
+var Jimp = require('jimp');
+
+// setInterval(tweetIt, 86400000);
+setInterval(tweetIt, 3000);
+
+function gecTime () {
+    var gec = 10;
+    gec = gec * (Math.pow(10, counter));
+    counter++;
+    return gec
+}
+
+function tweetIt() {
+    var gecNumber = gecTime();
+    var gecStatus = gecNumber.toString() + " gecs";
+    var filename = 'images/gectree.jpg'
+    var params = { encoding: 'base64' }
+    var b64 = fs.readFileSync(filename, params)
+        
+    T.post('media/upload', { media_data: b64 }, uploaded)
+    
+    function uploaded(err, data, response) {
+        //this is where I tweet!
+        var id = data.media_id_string
+        var tweet = {
+            status: gecStatus,
+            media_ids: [id]
+        }
+    T.post('statuses/update', tweet, tweeted)
+    }
+            
+    function tweeted (err,data,response) {
+        if (err) {
+            console.log("something wrong :-/");
+        }
+        else {
+            console.log(data.text);
+        }
+    }
+}
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'MJjqMIse176kD7ub2Y6jM7qSs',
+    consumer_secret:      'd8UdBdp0nvB7AwOVGUb5aKuhRUCwGKwbqxAccJ9iHMPaDOYox9',
+    access_token:         '1124643892924563456-Nh070yo4b2M46gxWucdmGeti11t1J6',
+    access_token_secret:  'zAwaGuKeykoYuvfkIC10CBs4WV4ZpsXPqGjNpaz4Hwy59'
+}
